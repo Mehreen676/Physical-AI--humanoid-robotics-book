b4fefcf65fb1cfd96a493f072e6588e4bc1b7342
Mehreen676 <mehreen@example.com>
2025-12-26 19:59:53 +0500

    Update .gitignore to exclude build artifacts and .docusaurus cache

--- front-end/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\Lenovo\\Desktop\\hackathon-physical-ai-humanoid-textbook-main\\docusaurus_textbook\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\Lenovo\\Desktop\\hackathon-physical-ai-humanoid-textbook-main\\docusaurus_textbook\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\Lenovo\\Desktop\\hackathon-physical-ai-humanoid-textbook-main\\docusaurus_textbook\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\Lenovo\\Desktop\\hackathon-physical-ai-humanoid-textbook-main\\docusaurus_textbook\\src\\css\\custom.css"),
-];
